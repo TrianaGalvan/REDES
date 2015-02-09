b197fba435102fbdb80af47190f0d3532b63f392
TrianaGalvan <trianagalvan@gmail.com>
2015-02-08 18:28:46 -0600

    completar la funcion structToArray
    ERROR,RRQ

--- include/tftp.c
+++ include/tftp.c
@@ -10,7 +10,7 @@
 int structToArray(Datagrama* datagrama,char** trama){
 	//pedir memoria para mi arreglo
 	int numBytes = 0;
-	int indice; 
+	int indice = 0; 
 	
 	*trama = (char*)calloc(1,sizeof(Datagrama));
 
@@ -22,22 +22,59 @@ int structToArray(Datagrama* datagrama,char** trama){
 	printf("\ntrama despues del if= %p\n",trama);
 	//direccion
 	*trama[0] = datagrama->tid;
+	indice +=1;
 	numBytes++;	
 	//opcode
 	*trama[1] = (char)datagrama->formato.opcode;
-	numBytes++;
-	
-	switch(*trama[1]){
-		case OPCODE_DATA:
+	indice+=2;
+	numBytes+=2;
+		
+	switch(datagrama->formato.opcode){
+		case OPCODE_DATA:	
 			break;
 		case OPCODE_ERR:
+			ERROR_TRAMA *error;
+			error = (ERROR_TRAMA*) &(datagrama->formato);
+			
+			//copiar codigo de error 
+			memcpy((*trama)+indice, &(error->errorCode), 2);
+			indice+=2;
+			numBytes += 2;
+			
+			//copiar mensaje de error
+			memcpy((*trama)+indice,&(error->errosMsg),strlen(error->errosMsg));
+			numBytes += strlen(error->errosMsg);
+			
 			break;
 		case OPCODE_RRQ:
+			int strlenFileName;
+			int strlenMode;
+			RRQ *rrq;
+			
+			rrq = (RRQ*) &(datagrama->formato);
+			
+			strlenFileName = strlen(rrq->fileName);
+			strlenMode = strlen(rrq->mode);
+			
+			memcpy((*trama)+indice,rrq->fileName,strlenFileName);
+			numBytes += strlenFileName;
+			indice += strlenFileName; 	
+		
+			*trama[indice] = 0;
+			numBytes++;
+			indice += 1;
+			
+			memcpy((*trama)+indice, rrq->mode,strlenMode);
+			numBytes += strlenMode;
+			indice+= strlenMode;
+			
+			*trama[indice] = 0;
+			
 			break;
 		case OPCODE_WRQ:
 			WRQ *wrq;
 			wrq = (WRQ*) &(datagrama->formato);
-			indice = 2;	
+
 			memcpy((*trama)+indice,wrq->fileName,strlen(wrq->fileName));
 			numBytes += strlen(wrq->fileName);
 			
